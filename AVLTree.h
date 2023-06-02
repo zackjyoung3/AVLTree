269c37cf3d7f96f6f767445c9a5077123df191e2
zackjyoung3 <zjy@georgetown.edu>
2023-06-02 12:43:16 -0400

    starting to make the outline for the avl tree and making a pretty print function so that I can visualize the progress

---
+++ AVLTree.h
@@ -0,0 +1,8 @@
+//
+// Created by Nicole Young on 6/2/23.
+//
+
+#ifndef AVL_AVLTREE_H
+#define AVL_AVLTREE_H
+
+#endif //AVL_AVLTREE_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "AVLTree.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
