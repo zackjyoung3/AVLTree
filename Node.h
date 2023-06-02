14a46d1c29da47fcb2711475895c624fa29ce843
zackjyoung3 <zjy@georgetown.edu>
2023-06-02 12:24:38 -0400

    Creating a Node struct with template parameters K and V that will serve as a node in an AVL Tree

---
+++ Node.h
@@ -0,0 +1,16 @@
+#ifndef AVL_NODE_H
+#define AVL_NODE_H
+
+template <typename K, typename V>
+struct Node {
+    Node(const K& key, const V& value)
+            : key(key), value(value), left(nullptr), right(nullptr), h(0) {}
+    K key;
+    V value;
+    Node* left;
+    Node* right;
+    int h;
+};
+
+
+#endif //AVL_NODE_H
